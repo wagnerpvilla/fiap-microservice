ced0d41f6d38325393912b4a4332c0591e7d0e81
Wagner Villa <wagner.pvilla@gmail.com>
2021-06-14 09:50:09 -0300

    send messages

--- atividade-02/GeekBurger/GeekBurger/Program.cs
+++ atividade-02/GeekBurger/GeekBurger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,10 +10,11 @@ namespace GeekBurger
 {
     class Program
     {
-        const string QueueConnectionString = "l+/ahOyFlKJ2BduLmg/1i1RPGRj0gjt/dtA4GuHgnAg=";
+        const string QueueConnectionString = "Endpoint=sb://geekburgervillawa.servicebus.windows.net/;SharedAccessKeyName=ProductPolicy;SharedAccessKey=l+/ahOyFlKJ2BduLmg/1i1RPGRj0gjt/dtA4GuHgnAg=";
         const string QueuePath = "productchanged";
         static IQueueClient _queueClient;
-
+        private List<Task> PendingCompleteTasks = new List<Task>();
+        int count = 0;
 
         static void Main(string[] args)
         {
@@ -42,35 +44,84 @@ namespace GeekBurger
 
         private async Task SendMessagesAsync()
         {
-            _queueClient = new QueueClient(QueueConnectionString, QueuePath);
-            var messages = "Hi,Hello,Hey,How are you,Be Welcome"
+            _queueClient = new QueueClient(QueueConnectionString, QueuePath)
+            {
+                OperationTimeout = TimeSpan.FromSeconds(10)
+            };
+            var messages = " Hi,Hello,Hey,How are you,Be Welcome"
                 .Split(',')
                 .Select(msg =>
                 {
                     Console.WriteLine($"Will send message: {msg}");
                     return new Message(Encoding.UTF8.GetBytes(msg));
-                }).ToList();
+                })
+                .ToList();
+            var sendTask = _queueClient.SendAsync(messages);
+            await sendTask;
+            CheckCommunicationExceptions(sendTask);
+            var closeTask = _queueClient.CloseAsync();
+            await closeTask;
+            CheckCommunicationExceptions(closeTask);
 
-            await _queueClient.SendAsync(messages);
-            await _queueClient.CloseAsync();
         }
+
+        public bool CheckCommunicationExceptions(Task task)
+        {
+            if (task.Exception == null || !task.Exception.InnerExceptions.Any())
+            {
+                return true;
+            }
+
+            task.Exception.InnerExceptions.ToList()
+                .ForEach(innerException =>
+                {
+                    Console.WriteLine($"Error in SendAsync task: {innerException.Message}. Details: {innerException.StackTrace}");
+
+                    if (innerException is ServiceBusCommunicationException)
+                    {
+                        Console.WriteLine("Connection Problem with Host");
+                    }
+                });
+
+            return false;
+        }
+
 
         private async Task ReceiveMessagesAsync()
         {
-            _queueClient = new QueueClient(QueueConnectionString, QueuePath);
-            _queueClient.RegisterMessageHandler(MessageHandler, new MessageHandlerOptions(ExceptionHandler)
-            {
-                AutoComplete = false
-            });
-
+            _queueClient = new QueueClient(QueueConnectionString, QueuePath, ReceiveMode.PeekLock);
+            _queueClient.RegisterMessageHandler(MessageHandler, new MessageHandlerOptions(ExceptionHandler) { AutoComplete = false });
             Console.ReadLine();
-            await _queueClient.CloseAsync();
+            Console.WriteLine($" Request to close async. Pending tasks: {PendingCompleteTasks.Count}");
+            await Task.WhenAll(PendingCompleteTasks);
+            Console.WriteLine($"All pending tasks were completed");
+            var closeTask = _queueClient.CloseAsync();
+            await closeTask;
+            CheckCommunicationExceptions(closeTask);
+
         }
 
         private async Task MessageHandler(Message message, CancellationToken cancellationToken)
         {
-            Console.WriteLine($"Received message:{Encoding.UTF8.GetString(message.Body)}");
-            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            Console.WriteLine($"Received message {Encoding.UTF8.GetString(message.Body)}");
+
+            if (cancellationToken.IsCancellationRequested || _queueClient.IsClosedOrClosing)
+            {
+                return;
+            }
+
+            Console.WriteLine($"task {count++}");
+            Task PendingTask;
+            lock (PendingCompleteTasks)
+            {
+                PendingCompleteTasks.Add(_queueClient.CompleteAsync(message.SystemProperties.LockToken));
+                PendingTask = PendingCompleteTasks.LastOrDefault();
+            }
+            Console.WriteLine($"calling complete for task {count}");
+            await PendingTask;
+            Console.WriteLine($"remove task {count} from task queue");
+            PendingCompleteTasks.Remove(PendingTask);
+
         }
 
         private Task ExceptionHandler(ExceptionReceivedEventArgs exceptionArgs)
